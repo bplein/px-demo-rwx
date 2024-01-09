baa3dab6eb2cee1fc8d5555f01d7bb761d33b723
Bill Plein <plein@plein-jump.fsa.lab>
2024-01-09 14:49:58 +0000

    updated

--- exec_pod.sh
+++ exec_pod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-BB_POD=$(kubectl get pods -l app=busybox-rwx -o name | head -n 1)
-kubectl exec -ti $BB_POD -- cat /mnt/busybox-rwx/status.txt
+BB_POD=$(kubectl get pods -l app=busybox-rwx-px -o name | head -n 1)
+kubectl exec -ti $BB_POD -- tail -f /mnt/busybox-rwx/status.txt
