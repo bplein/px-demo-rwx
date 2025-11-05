dbb2bd0c23468d9b4d674345a579aec4ddcb20c6
Bill Plein <260078+bplein@users.noreply.github.com>
2025-11-05 09:14:59 -0600

    General updates
    Add fa-file, rename YAML to better reflect types, update to write to one file per pod and watch the files grow over time.

--- exec_pod.sh
+++ exec_pod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 BB_POD=$(kubectl get pods -l app=busybox-rwx-px -o name | head -n 1)
-kubectl exec -ti $BB_POD -- tail -f /mnt/busybox-rwx/status.txt
+kubectl exec -ti $BB_POD -- watch ls -la /mnt/busybox-rwx/
